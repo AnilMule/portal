48dabfa1c277f90551a4df20c93cc35b1d50ff4f
AnilMule <anilkumarcholla@gmail.com>
2017-12-11 15:35:11 +0530

    fixed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { AddflightComponent } from './addflight/addflight.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent,
-    AddflightComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
